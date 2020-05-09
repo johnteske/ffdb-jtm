61186d1b8b649a26f82e70d03d252781be21ed4e
John Teske <john.teske@protonmail.com>
2020-05-09 11:49:07 -0700

    Test date mapping

--- tests.js
+++ tests.js
@@ -2,6 +2,12 @@ const test = require("tape-promise/tape");
 
 const { getPerformances } = require("./index");
 
-test("timing test", async (t) => {
+test("getPerformances", async (t) => {
+  // getter
   await t.doesNotReject(getPerformances);
+
+  // TODO full schema test
+  const performances = await getPerformances();
+  const first = performances[0]
+  t.equal(first.date.length, 8);
 });
